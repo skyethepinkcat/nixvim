079e95ded8920c25d851811bc08fad323b0a96ec
Skye J <skye@skyenet.online>
2026-02-06 13:37:32 -0500

    working on implementing nvix

--- flake.nix
+++ flake.nix
@@ -4,11 +4,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixvim.url = "github:nix-community/nixvim/nixos-25.11";
+    nvix.url = "github:niksingh710/nvix";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    { nixvim, flake-parts, ... }@inputs:
+    { nixvim, flake-parts, nvix, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
@@ -24,7 +25,10 @@
           nixvim' = nixvim.legacyPackages.${system};
           nixvimModule = {
             inherit system; # or alternatively, set `pkgs`
-            module = import ./config; # import the module directly
+            modules = [
+            import ./config # import the module directly
+            nvix.nvixPlugins.default
+            ]
             # You can use `extraSpecialArgs` to pass additional arguments to your module files
             extraSpecialArgs = {
               # inherit (inputs) foo;
