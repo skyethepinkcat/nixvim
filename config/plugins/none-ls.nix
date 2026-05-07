e2eda2334ab74d9c01b898ef436d20902f1b5975
Skye J <skye@skyenet.online>
2026-05-07 09:44:46 -0400

    fix(yamllint): specify default args

--- config/plugins/none-ls.nix
+++ config/plugins/none-ls.nix
@@ -13,7 +13,54 @@ in
             enable = true;
             package = null;
           };
-          yamllint.enable = true;
+          yamllint = {
+            enable = true;
+            settings = {
+              extra_args = [
+                "-d"
+                (lib.strings.toJSON {
+                  yaml-files = [
+                    "*.yaml"
+                    "*.yml"
+                    ".yamllint"
+                  ];
+                  rules = {
+                    anchors = "enable";
+                    braces = "enable";
+                    brackets = "enable";
+                    colons = "enable";
+                    commas = "enable";
+                    comments = {
+                      level = "warning";
+                    };
+                    comments-indentation = {
+                      level = "warning";
+                    };
+                    document-end = "disable";
+                    document-start = {
+                      level = "warning";
+                    };
+                    empty-lines = "enable";
+                    empty-values = "disable";
+                    float-values = "disable";
+                    hyphens = "enable";
+                    indentation = "enable";
+                    key-duplicates = "enable";
+                    key-ordering = "disable";
+                    line-length = "disable";
+                    new-line-at-end-of-file = "enable";
+                    new-lines = "enable";
+                    octal-values = "disable";
+                    quoted-strings = "disable";
+                    trailing-spaces = "enable";
+                    truthy = {
+                      level = "warning";
+                    };
+                  };
+                })
+              ];
+            };
+          };
           puppet_lint = {
             enable = true;
             package = null;
