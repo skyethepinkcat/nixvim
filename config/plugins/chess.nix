676222c8cb0cc46eb5413962e7999777761e2f8e
Skye Jonke <skye@skyejonke.com>
2026-05-11 14:52:48 -0400

    feat: added nvim-chess (#23)
    * feat: added nvim-chess
    
    * fix(chess): added enable option
    
    * chore(chess): formatting

---
+++ config/plugins/chess.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+{
+  options.plugins.chess.enable = lib.mkEnableOption "nvim-chess";
+
+  config = lib.mkIf config.plugins.chess.enable {
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "chess";
+        src = pkgs.fetchFromGitHub {
+          repo = "nvim-chess";
+          owner = "linuxswords";
+          rev = "v0.6.0";
+          hash = "sha256-YIkdXLDbU8ThrLwc0JXefkbm45K+/auoPmpVtj2Djw0=";
+        };
+        dependencies = [ pkgs.vimPlugins.plenary-nvim ];
+      })
+    ];
+    extraConfigLua =
+      # lua
+      ''
+        require("nvim-chess").setup({})
+      '';
+  };
+}
