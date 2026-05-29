7d5f5997de2232b784bc3a2f49ce9f0e119a34a0
Skye J <skye@skyenet.online>
2026-05-29 09:36:43 -0400

    fix: switch to unstable nixvim

--- flake.nix
+++ flake.nix
@@ -11,8 +11,7 @@
     };
 
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-26.05";
-      # inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/nixvim";
     };
 
     flake-parts.url = "github:hercules-ci/flake-parts";
