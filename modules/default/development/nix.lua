e100b8f30c980c7972abb4715970140a6981cd38
Skye J <skye@skyenet.online>
2026-08-07 10:19:22 -0400

    fix: working on eval

--- modules/default/development/nix.lua
+++ modules/default/development/nix.lua
@@ -1,9 +1,31 @@
-local M = { output = nil }
+--- @class nix
+local M = {
+	output = nil,
+	nix_path = "/run/current-system/sw/bin/nix",
+	flake = ".",
+	jq_path = "jq",
+}
+
+local function dump(o)
+	if type(o) == "table" then
+		local s = "{ "
+		for k, v in pairs(o) do
+			if type(k) ~= "number" then
+				k = '"' .. k .. '"'
+			end
+			s = s .. "[" .. k .. "] = " .. dump(v) .. ","
+		end
+		return s .. "} "
+	else
+		return tostring(o)
+	end
+end
 
 local api = vim.api
 
 local ts = vim.treesitter
 
+--- @param path string
 local function add_dot(path)
 	if path == "" then
 		return path
@@ -11,6 +33,8 @@ local function add_dot(path)
 	return path .. "."
 end
 
+--- @param node vim.treesitter.LanguageTree
+--- @param fullpath string
 local function print_node(node, fullpath)
 	if node:type() == "binding" then
 		local attrpath = node:field("attrpath")[1]
@@ -21,6 +45,7 @@ local function print_node(node, fullpath)
 	return fullpath
 end
 
+-- @param TSNODE
 local function concat_nodes(root, dest, fullPath)
 	if root:type() == "list_expression" then
 		return fullPath
@@ -41,14 +66,43 @@ function M.get_option_path()
 	return concat_nodes(root, node, "config")
 end
 
+function M.print_config_path()
+	local path = M.get_option_path()
+	vim.notify(path)
+end
+
 function M.copy_config_path()
 	local path = M.get_option_path()
 	vim.notify("Copied: " .. path)
 	vim.fn.setreg("+", path)
 end
 
+function M.nix_eval(flake_output, apply)
+	local apply_command = ""
+	if apply ~= nil and apply ~= "" then
+		apply_command = string.format('--apply "%s"', apply)
+	end
+
+	local result = vim.system({
+		"bash",
+		"-c",
+		string.format("%s eval %s#%s %s --json", M.nix_path, M.flake, flake_output, apply_command),
+	}, { text = true }):wait()
+
+	if result.code ~= 0 then
+		-- See https://github.com/NixOS/nix/issues/11576
+		vim.notify("Unable to evaluate as JSON, trying regular eval...", "warn")
+		return vim.system({
+			"bash",
+			"-c",
+			string.format("%s eval %s#%s %s", M.nix_path, M.flake, flake_output, apply_command),
+		}, { text = true }):wait()
+	end
+	return result
+end
+
 function M.set_output(setting)
-	if setting ~= nil then
+	if setting == nil then
 		vim.ui.input({ prompt = "Enter the nix output to use.", scope = "project" }, function(input)
 			M.output = input
 		end)
@@ -83,26 +137,19 @@ function M.eval_config()
 		M.set_output(nil)
 	end
 	if M.output ~= nil then
-		local full_path = M.output .. "." .. M.get_option_path()
-		local result = vim.system({
-			"bash",
-			"-c",
-			string.format(
-				'/run/current-system/sw/bin/nix eval .#%s.config --apply "config: %s" --json | jq',
-				M.output,
-				M.get_option_path()
-			),
-		}, {
-			text = true,
-		}):wait()
+		local result = M.nix_eval(string.format(M.output .. "." .. M.get_option_path()), nil)
 		if result.code == 0 then
 			local buf = vim.api.nvim_create_buf(false, true) -- listed=false, scratch=true
+			local formatted_json = vim.system(M.jq_path, { text = true }):write(result.stdout):wait()
 			vim.api.nvim_open_win(buf, true, window_config())
 
+			vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(formatted_json, "\n"))
 			vim.api.nvim_set_option_value("filetype", "json", { buf = buf })
-			vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(result.stdout, "\n"))
+			vim.api.nvim_set_option_value("readonly", true, { buf = buf })
+			vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
+			vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = buf })
 		else
-			vim.notify(result.stderr)
+			vim.notify(string.format("Unable to evaluate:\n%s", result.stderr), "error")
 		end
 	end
 end
--- modules/default/development/nix.nix
+++ modules/default/development/nix.nix
@@ -1,17 +1,28 @@
-{ lib, utils, ... }:
+{ pkgs, lib, utils, ... }:
 let
   inherit (utils) mkFunc;
 in
 {
   extraFiles."lua/nix.lua".source = ./nix.lua;
+  extraConfigLua = ''
+    require('nix').jq_path = "${lib.getExe pkgs.jq}";
+
+  '';
 
   ftKeyList.nix = [
     {
-      key = "<LocalLeader>n";
+      key = "<LocalLeader>c";
       action = mkFunc ''
         require('nix').copy_config_path()
       '';
       desc = "Copy the configuration path";
+    }
+    {
+      key = "<LocalLeader>p";
+      action = mkFunc ''
+        require('nix').copy_config_path()
+      '';
+      desc = "Display the configuration path";
     }
     {
       key = "<LocalLeader>=";
