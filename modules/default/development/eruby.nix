6b2cdec1bf996940d64550e3ea628fc0d6f73368
Skye J <skye@skyenet.online>
2026-08-13 15:13:25 -0400

    feat: added global erb files

---
+++ modules/default/development/eruby.nix
@@ -0,0 +1,29 @@
+{ lib, ... }: {
+  autoGroups = {
+    ErbFiles = { };
+  };
+  extraConfigLua = ''
+    vim.cmd('syntax on')
+  '';
+  autoCmd = [
+    {
+      event = "Syntax";
+      group = "ErbFiles";
+
+      callback = lib.nixvim.mkRaw ''
+        function(args)
+        local saved = vim.b.current_syntax
+        local name = vim.api.nvim_buf_get_name(args.buf)
+        if not name:match('%.erb$') then return end
+        vim.b.current_syntax = nil
+        vim.cmd([[
+          syntax include @Ruby syntax/ruby.vim
+          syntax region erbTag matchgroup=PreProc start="<%=\?-\?" end="-\?%>" contains=@Ruby containedin=ALL keepend
+          syntax region erbComment matchgroup=PreProc start=+<%#+ end=+%>+ containedin=ALL keepend
+        ]])
+        vim.b.current_syntax = saved
+        end
+      '';
+    }
+  ];
+}
